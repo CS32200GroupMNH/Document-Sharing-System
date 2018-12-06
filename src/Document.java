9c9f7c23dcc2a5b9f01b40059aa10cdb16403d73
NaeemMeman <naeemmeman@gmail.com>
2018-12-06 18:48:47 -0500

    updating checkDocumentForTabooWords

--- src/Document.java
+++ src/Document.java
@@ -45,11 +45,13 @@ public class Document {
     //checks document for taboo word and replaces with UNK, returns true if a words is found in the document
     public boolean checkDocumentForTabooWords()
     {
+        SystemManager s = SystemManager.getInstance();
+
         boolean isThereUNK = false;
 
         StringBuilder newDocumentContent = new StringBuilder("");
 
-        HashSet<String> tabooWordsList = getTabooWords(documentID); //getTabooWords(String documentID) returns a hashset from system manager
+        HashSet<String> tabooWordsList = s.getTabooWords(documentID); //getTabooWords(String documentID) returns a hashset from system manager
         String[] documentWords = getDocumentContent().split("\n");
 
         for(int i = 0; i<documentWords.length; i++ )
